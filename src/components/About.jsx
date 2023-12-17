343b9ea9fdcd7b08d146b4cb7f624d0b228cc5f2
Ad-Amrit <amritadhikari.dev@gmail.com>
2023-12-17 13:02:26 +0545

    about me changed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div><h1>About me</h1></div>
+    <div>
+      <h1>About me</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio illo molestiae dignissimos suscipit quibusdam ullam animi 
+        autem aspernatur, deleniti exercitationem, ratione quasi eaque ipsam iure officia perspiciatis recusandae facere? Dolorum?</p>
+    </div>
+  
   )
 }
 
